0deb080692cf3a4008e1bcd82e036d010c2341a4
hussain naqvi <hussain31671@gmail.com>
2026-04-25 13:53:30 +0500

    add task container

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -68,8 +68,65 @@ class HomePage extends StatelessWidget {
                       borderRadius: BorderRadius.circular(12)
                     )
                   ), child: Text('+New Task',style: TextStyle(color: Colors.white),),),
+                      
                 ],
-              )
+              ),
+
+         ///  ----------------------------[card and lists sections] ---------------------------
+         Gap(20),
+         Container(
+          width: double.infinity,
+          height: 120,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(12),bottomLeft: Radius.circular(12))
+                ),
+                width: 15,
+              ),
+              Expanded(child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      title: Text('learning flutter development',),
+                      subtitle: Text('learn state management and backend'),
+                      trailing: Transform.scale(
+                        scale: 1.5,
+                        
+                        child: Checkbox(
+                          activeColor: Colors.blue.shade800,
+                          shape: CircleBorder(),
+                          value: true, onChanged: (value) {} )),
+                    ),
+                    Divider(
+                      thickness: 1.5,
+                      color: Colors.grey.shade400,
+                    ),
+
+                    Row(
+                      children: [
+                        Text('Today'),
+                        Gap(12),
+                        Text('9:12 AM')
+                      ],
+                    )
+                  ],
+                ),
+              ))
+            ],
+          ),
+         )
+
             ],
           ),
         ),
